00207908361561c6f94b1e03b551b4d6e75fb9b2
Charles Leifer <coleifer@gmail.com>
2013-05-16 19:30:38 -0500

    Cleaning up orm

--- wtfpeewee/orm.py
+++ wtfpeewee/orm.py
@@ -2,41 +2,61 @@
 Tools for generating forms based on Peewee models
 (cribbed from wtforms.ext.django)
 """
+from collections import namedtuple
 from decimal import Decimal
-from wtforms import fields as f
 from wtforms import Form
-from wtforms import validators, ValidationError
-from wtfpeewee.fields import ModelSelectField, SelectChoicesField, WPTimeField,\
-    WPDateField, WPDateTimeField
+from wtforms import fields as f
+from wtforms import ValidationError
+from wtforms import validators
+from wtfpeewee.fields import ModelSelectField
+from wtfpeewee.fields import SelectChoicesField
+from wtfpeewee.fields import WPDateField
+from wtfpeewee.fields import WPDateTimeField
+from wtfpeewee.fields import WPTimeField
 
-from peewee import PrimaryKeyField, IntegerField, FloatField, DateTimeField,\
-    BooleanField, CharField, TextField, ForeignKeyField, DecimalField, DateField,\
-    TimeField, DoesNotExist, DoubleField
+from peewee import BlobField
+from peewee import BooleanField
+from peewee import CharField
+from peewee import DateField
+from peewee import DateTimeField
+from peewee import DecimalField
+from peewee import DoesNotExist
+from peewee import DoubleField
+from peewee import FloatField
+from peewee import ForeignKeyField
+from peewee import IntegerField
+from peewee import PrimaryKeyField
+from peewee import TextField
+from peewee import TimeField
 
 
 __all__ = (
-    'model_fields', 'model_form',
-)
+    'FieldInfo',
+    'ModelConverter',
+    'model_fields',
+    'model_form')
 
 def handle_null_filter(data):
     if data == '':
         return None
     return data
 
+FieldInfo = namedtuple('FieldInfo', ('name', 'field'))
 
 class ModelConverter(object):
     defaults = {
-        PrimaryKeyField: f.HiddenField,
-        IntegerField: f.IntegerField,
-        FloatField: f.FloatField,
-        DoubleField: f.FloatField,
-        DecimalField: f.DecimalField,
-        DateTimeField: WPDateTimeField,
-        DateField: WPDateField,
-        TimeField: WPTimeField,
+        BlobField: f.TextAreaField,
         BooleanField: f.BooleanField,
         CharField: f.TextField,
+        DateField: WPDateField,
+        DateTimeField: WPDateTimeField,
+        DecimalField: f.DecimalField,
+        DoubleField: f.FloatField,
+        FloatField: f.FloatField,
+        IntegerField: f.IntegerField,
+        PrimaryKeyField: f.HiddenField,
         TextField: f.TextAreaField,
+        TimeField: WPTimeField,
     }
     coerce_defaults = {
         IntegerField: int,
@@ -44,12 +64,15 @@ class ModelConverter(object):
         CharField: unicode,
         TextField: unicode,
     }
-    required = (DateTimeField, CharField, TextField, ForeignKeyField, PrimaryKeyField)
+    required = (
+        CharField,
+        DateTimeField,
+        ForeignKeyField,
+        PrimaryKeyField,
+        TextField)
 
     def __init__(self, additional=None, additional_coerce=None):
-        self.converters = {
-            ForeignKeyField: self.handle_foreign_key,
-        }
+        self.converters = {ForeignKeyField: self.handle_foreign_key}
         if additional:
             self.converters.update(additional)
 
@@ -64,49 +87,59 @@ class ModelConverter(object):
             field_obj = SelectQueryField(query=field.choices, **kwargs)
         else:
             field_obj = ModelSelectField(model=field.rel_model, **kwargs)
-        return field.name, field_obj
+        return FieldInfo(field.name, field_obj)
 
     def convert(self, model, field, field_args):
-        kwargs = dict(
-            label=field.verbose_name,
-            validators=[],
-            filters=[],
-            default=field.default,
-            description=field.help_text,
-        )
+        kwargs = {
+            'label': field.verbose_name,
+            'validators': [],
+            'filters': [],
+            'default': field.default,
+            'description': field.help_text}
         if field_args:
             kwargs.update(field_args)
 
         if field.null:
+            # Treat empty string as None when converting.
             kwargs['filters'].append(handle_null_filter)
 
-        if field.null or (field.default is not None):
+        if (field.null or (field.default is not None)) and not field.choices:
+            # If the field can be empty, or has a default value, do not require
+            # it when submitting a form.
             kwargs['validators'].append(validators.Optional())
         else:
             if isinstance(field, self.required):
                 kwargs['validators'].append(validators.Required())
 
         field_class = type(field)
-
         if field_class in self.converters:
             return self.converters[field_class](model, field, **kwargs)
         elif field_class in self.defaults:
             if issubclass(self.defaults[field_class], f.FormField):
+                # FormField fields (i.e. for nested forms) do not support
+                # filters.
                 kwargs.pop('filters')
             if field.choices or 'choices' in kwargs:
                 choices = kwargs.pop('choices', field.choices)
                 if field_class in self.coerce_settings or 'coerce' in kwargs:
-                    coerce_fn = kwargs.pop('coerce', self.coerce_settings[field_class])
+                    coerce_fn = kwargs.pop('coerce',
+                                           self.coerce_settings[field_class])
                     allow_blank = kwargs.pop('allow_blank', field.null)
-                    kwargs.update(dict(choices=choices, coerce=coerce_fn, allow_blank=allow_blank))
-                    return field.name, SelectChoicesField(**kwargs)
+                    kwargs.update({
+                        'choices': choices,
+                        'coerce': coerce_fn,
+                        'allow_blank': allow_blank})
 
-            return field.name, self.defaults[field_class](**kwargs)
+                    return FieldInfo(field.name, SelectChoicesField(**kwargs))
 
-        raise AttributeError('There is not possible conversion for \'%s\'' % type(field))
+            return FieldInfo(field.name, self.defaults[field_class](**kwargs))
+
+        raise AttributeError("There is not possible conversion "
+                             "for '%s'" % field_class)
 
 
-def model_fields(model, allow_pk=False, only=None, exclude=None, field_args=None, converter=None):
+def model_fields(model, allow_pk=False, only=None, exclude=None,
+                 field_args=None, converter=None):
     """
     Generate a dictionary of fields for a given Peewee model.
 
@@ -116,26 +149,24 @@ def model_fields(model, allow_pk=False, only=None, exclude=None, field_args=None
     field_args = field_args or {}
 
     model_fields = list(model._meta.get_sorted_fields())
-
     if not allow_pk:
         model_fields.pop(0)
 
     if only:
-        model_fields = (x for x in model_fields if x[0] in only)
+        model_fields = [x for x in model_fields if x[0] in only]
     elif exclude:
-        model_fields = (x for x in model_fields if x[0] not in exclude)
+        model_fields = [x for x in model_fields if x[0] not in exclude]
 
     field_dict = {}
     for name, model_field in model_fields:
-        field_info = converter.convert(model, model_field, field_args.get(name))
-        if field_info is not None:
-            field_name, field_obj = field_info
-            field_dict[field_name] = field_obj
+        info = converter.convert(model, model_field, field_args.get(name))
+        field_dict[info.name] = info.field
 
     return field_dict
 
 
-def model_form(model, base_class=Form, allow_pk=False, only=None, exclude=None, field_args=None, converter=None):
+def model_form(model, base_class=Form, allow_pk=False, only=None, exclude=None,
+               field_args=None, converter=None):
     """
     Create a wtforms Form for a given Peewee model class::
 
