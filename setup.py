32d7f23fae5758a08d3b694edcffc7effcd55395
Charles Leifer <coleifer@gmail.com>
2015-12-01 22:02:50 -0600

    0.2.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='wtf-peewee',
-    version='0.2.5',
+    version='0.2.6',
     url='https://github.com/coleifer/wtf-peewee/',
     license='MIT',
     author='Charles Leifer',
