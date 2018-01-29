6c0f8e17a13846f5d6b01d4eab765a76bec9ffcf
Charles Leifer <coleifer@gmail.com>
2018-01-29 09:49:43 -0600

    Peewee version requirements.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'peewee>=2.0.0', 'wtforms',
+        'peewee>=3.0.0', 'wtforms',
     ],
     classifiers=[
         'Environment :: Web Environment',
